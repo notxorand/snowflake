2ff0267a99656561c8ea3d6e260a2bd57cd338e3
notxorand <ewanretorokugbe@gmail.com>
2026-06-11 18:07:20 +0100

    remove redundant ignis config

--- sources/sym/ignis/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .launcher import Launcher
-from .notification_popup import NotificationPopup
-from .control_center import ControlCenter
-
-__all__ = ['Launcher', 'NotificationPopup', 'ControlCenter']
